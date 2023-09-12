125c0a9f7520b0911492925823ecf0294c26ae3b
prkvw <neeqash@gmail.com>
2023-09-12 06:27:53 +0000

    Init

---
+++ packages/nextjs/components/Header.tsx
@@ -0,0 +1,128 @@
+import React, { useCallback, useRef, useState } from "react";
+import Image from "next/image";
+import Link from "next/link";
+import { useRouter } from "next/router";
+import {
+  ArrowDownTrayIcon,
+  ArrowPathIcon,
+  ArrowUpTrayIcon,
+  Bars3Icon,
+  BugAntIcon,
+  MagnifyingGlassIcon,
+  PhotoIcon,
+} from "@heroicons/react/24/outline";
+import { FaucetButton, RainbowKitCustomConnectButton } from "~~/components/scaffold-eth";
+import { useOutsideClick } from "~~/hooks/scaffold-eth";
+
+const NavLink = ({ href, children }: { href: string; children: React.ReactNode }) => {
+  const router = useRouter();
+  const isActive = router.pathname === href;
+
+  return (
+    <Link
+      href={href}
+      passHref
+      className={`${
+        isActive ? "bg-secondary shadow-md" : ""
+      } hover:bg-secondary hover:shadow-md focus:!bg-secondary py-1.5 px-2 text-sm rounded-full gap-1.5`}
+    >
+      {children}
+    </Link>
+  );
+};
+
+/**
+ * Site header
+ */
+export const Header = () => {
+  const [isDrawerOpen, setIsDrawerOpen] = useState(false);
+  const burgerMenuRef = useRef<HTMLDivElement>(null);
+  useOutsideClick(
+    burgerMenuRef,
+    useCallback(() => setIsDrawerOpen(false), []),
+  );
+
+  const navLinks = (
+    <>
+      <li>
+        <NavLink href="/myNFTs">
+          <PhotoIcon className="h-4 w-4" />
+          My NFTs
+        </NavLink>
+      </li>
+      <li>
+        <NavLink href="/transfers">
+          <ArrowPathIcon className="h-4 w-4" />
+          Transfers
+        </NavLink>
+      </li>
+      <li>
+        <NavLink href="/ipfsUpload">
+          <ArrowUpTrayIcon className="h-4 w-4" />
+          IPFS Upload
+        </NavLink>
+      </li>
+      <li>
+        <NavLink href="/ipfsDownload">
+          <ArrowDownTrayIcon className="h-4 w-4" />
+          IPFS Download
+        </NavLink>
+      </li>
+      <li>
+        <NavLink href="/debug">
+          <BugAntIcon className="h-4 w-4" />
+          Debug Contracts
+        </NavLink>
+      </li>
+      <li>
+        <NavLink href="/blockexplorer">
+          <MagnifyingGlassIcon className="h-4 w-4" />
+          Block Explorer
+        </NavLink>
+      </li>
+    </>
+  );
+
+  return (
+    <div className="sticky xl:static top-0 navbar bg-primary min-h-0 flex-shrink-0 justify-between z-20 shadow-md shadow-secondary px-0 sm:px-2">
+      <div className="navbar-start w-auto xl:w-1/2">
+        <div className="xl:hidden dropdown" ref={burgerMenuRef}>
+          <label
+            tabIndex={0}
+            className={`ml-1 btn btn-ghost ${isDrawerOpen ? "hover:bg-secondary" : "hover:bg-transparent"}`}
+            onClick={() => {
+              setIsDrawerOpen(prevIsOpenState => !prevIsOpenState);
+            }}
+          >
+            <Bars3Icon className="h-1/2" />
+          </label>
+          {isDrawerOpen && (
+            <ul
+              tabIndex={0}
+              className="menu menu-compact dropdown-content mt-3 p-2 shadow bg-primary rounded-box w-52"
+              onClick={() => {
+                setIsDrawerOpen(false);
+              }}
+            >
+              {navLinks}
+            </ul>
+          )}
+        </div>
+        <Link href="/" passHref className="hidden xl:flex items-center gap-1 ml-4 mr-6 shrink-0">
+          <div className="flex relative w-10 h-10">
+            <Image alt="SE2 logo" className="cursor-pointer" fill src="/logo.svg" />
+          </div>
+          <div className="flex flex-col">
+            <span className="font-bold leading-tight">SRE Challenges</span>
+            <span className="text-xs">#0: Simple NFT</span>
+          </div>
+        </Link>
+        <ul className="hidden xl:flex xl:flex-nowrap menu menu-horizontal px-1 gap-2">{navLinks}</ul>
+      </div>
+      <div className="navbar-end flex-grow mr-4">
+        <RainbowKitCustomConnectButton />
+        <FaucetButton />
+      </div>
+    </div>
+  );
+};
